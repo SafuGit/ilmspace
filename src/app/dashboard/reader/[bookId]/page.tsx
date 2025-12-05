3dc6bf05d5859d209b4be14fb664b055f62f175a
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-12-05 17:53:11 +0600

    feat(PDFViewer): add PdfViewer component and integrate react-pdf library

---
+++ src/app/dashboard/reader/[bookId]/page.tsx
@@ -0,0 +1,8 @@
+import PdfViewer from '@/components/PDFViewer/PdfViwer';
+import React from 'react';
+
+const PdfViewerPage = () => {
+  return <PdfViewer />
+};
+
+export default PdfViewerPage;
---
+++ src/components/PDFViewer/PdfViwer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const PdfViewer = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default PdfViewer;
