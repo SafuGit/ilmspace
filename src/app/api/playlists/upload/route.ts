2e5a9129fe9808ba9fb71b62d06cbc8d5cfaf6d1
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-12-13 12:07:46 +0600

    feat(upload): enhance error handling for playlist creation in POST route

--- src/app/api/playlists/upload/route.ts
+++ src/app/api/playlists/upload/route.ts
@@ -1,8 +1,18 @@
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    try {
+      const response = prisma.playlist.create(body);
+    } catch (error) {
+      console.error("Error creating playlist in database:", error);
+      return NextResponse.json(
+        { message: "Failed to create playlist" },
+        { status: 500 }
+      );
+    }
   } catch (error) {
     console.error("Error uploading playlist:", error);
     return NextResponse.json(
